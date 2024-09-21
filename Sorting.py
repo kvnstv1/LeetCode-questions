b98e11ae10c035464761834cf874c2cc11816174
Kevin Steve Sathyanath <satke569@student.otago.ac.nz>
2024-09-22 00:07:12 +1200

    bubblesort, mergesort and insertionsort added

--- Sorting.py
+++ Sorting.py
@@ -8,6 +8,14 @@ class Sorting:
     def __init__(self):
         pass
 
+    #Sorting covered:
+    # 1. Selection sort
+    # 2. Insertion sort
+    # 3. Bubble sort
+    # 4. Merge sort
+    # 5. Quick sort
+
+
     #Selection sort
     def selectionSort(self, listNums):
         unsorted = np.array(listNums)
@@ -24,6 +32,69 @@ class Sorting:
         return unsorted
 
 
+
+    #Insertion sort
+    def insertionSort(self, listNums):
+        #print("")
+        for i in range(1, len(listNums)-1):
+            key = listNums[i]
+            for j in range(i-1, -1, -1):
+                if(listNums[j] > key):
+                    listNums[j+1] = listNums[j]
+                    listNums[j] = key
+                    #self.printer(listNums)
+                    #print("")
+        return listNums;
+
+
+
+    #Bubble sort
+    def bubbleSort(self, listNums):
+        while(True):
+            swap = 0
+            for i in range(0, len(listNums)-1):
+                if(listNums[i]>listNums[i+1]):
+                    listNums[i], listNums[i+1] = listNums[i+1], listNums[i]
+                    swap += 1
+            if(swap == 0):
+                break
+        return listNums
+
+
+
+    # Merge sort
+    def mergeSort(self, listNums):
+
+
+
+        #Base case for recursion
+        if(len(listNums)<=1):
+            return listNums
+
+        mid = len(listNums)//2
+        left = listNums[:mid]
+        right = listNums[mid:]
+        left = self.mergeSort(left)
+        right = self.mergeSort(right)
+
+        return self.merge(left, right)
+
+    # Inner function
+    def merge(self, l, r):
+        result = []
+        i, j = 0,0
+        while (i<len(l) and j < len(r)):
+            if (l[i] <= r[j]):
+                result.append(l[i])
+                i += 1
+            else:
+                result.append(r[j])
+                j+=1
+        result.extend(l[i:])
+        result.extend(r[j:])
+        return result
+
+    # Display method
     def printer(self, listNums):
         for i in range(len(listNums)):
             print(f"{listNums[i]}", end = " ")
@@ -39,7 +110,7 @@ def main():
     #print the unsorted list
     print("\nUnsorted: ")
     Sorting_instance.printer(unsorted)
-    sorted = Sorting_instance.selectionSort(unsorted)
+    sorted = Sorting_instance.mergeSort(unsorted)
     #Print the sorted list
     print("\nSorted: ")
     Sorting_instance.printer(sorted)
